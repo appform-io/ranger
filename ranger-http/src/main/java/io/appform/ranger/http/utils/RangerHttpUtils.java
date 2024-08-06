0ba1be0706ef02ffb62844dc6adde3ef35ad3531
Santanu Sinha <santanu.sinha@gmail.com>
2024-08-06 13:58:13 +0530

    Optimized to create only one http client per http upstream

--- ranger-http/src/main/java/io/appform/ranger/http/utils/RangerHttpUtils.java
+++ ranger-http/src/main/java/io/appform/ranger/http/utils/RangerHttpUtils.java
@@ -2,6 +2,7 @@ package io.appform.ranger.http.utils;
 
 import io.appform.ranger.http.config.HttpClientConfig;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 
@@ -11,8 +12,10 @@ import java.util.concurrent.TimeUnit;
  * Set of utilities for http client
  */
 @UtilityClass
+@Slf4j
 public class RangerHttpUtils {
     public static OkHttpClient httpClient(final HttpClientConfig config) {
+        log.info("Creating http client for {}:{}", config.getHost(), config.getPort());
         return new OkHttpClient.Builder()
                 .callTimeout(config.getOperationTimeoutMs() == 0
                              ? 3000
