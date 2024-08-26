8caea77d1473ff3f91951547d63a3e8bf4737b72
Santanu Sinha <santanu.sinha@gmail.com>
2024-08-26 08:54:54 +0530

    Updated license

--- ranger-http/src/test/java/io/appform/ranger/http/ResourceHelper.java
+++ ranger-http/src/test/java/io/appform/ranger/http/ResourceHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Flipkart Internet Pvt. Ltd.
+ * Copyright 2024 Authors, Flipkart Internet Pvt. Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
