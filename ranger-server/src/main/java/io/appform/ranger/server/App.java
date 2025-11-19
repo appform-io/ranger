218547d84bf33a30e3cfdacac53976a595bb1de4
Phaneesh Nagaraja <phaneesh@phonepe.com>
2025-11-19 09:37:39 +0530

    Deprecate Dropwizard 2.x based ranger-server and replace it with Dropwizard 5.0.0 based HTTP server.

--- ranger-server/src/main/java/io/appform/ranger/server/App.java
+++ ranger-server/src/main/java/io/appform/ranger/server/App.java
@@ -23,9 +23,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.appform.ranger.hub.server.bundle.RangerHubServerBundle;
 import io.appform.ranger.hub.server.bundle.configuration.RangerServerConfiguration;
-import io.dropwizard.Application;
-import io.dropwizard.setup.Bootstrap;
-import io.dropwizard.setup.Environment;
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
 import lombok.SneakyThrows;
 import lombok.val;
 
@@ -36,7 +36,7 @@ public class App extends Application<AppConfig> {
 
     @Override
     public void initialize(Bootstrap<AppConfig> bootstrap) {
-        bootstrap.addBundle(new RangerHubServerBundle<AppConfig>() {
+        bootstrap.addBundle(new RangerHubServerBundle<>() {
             @Override
             protected RangerServerConfiguration getRangerConfiguration(AppConfig configuration) {
                 return configuration.getRanger();
@@ -48,8 +48,8 @@ public class App extends Application<AppConfig> {
     public void run(AppConfig appConfig, Environment environment) throws Exception {
         val objectMapper = environment.getObjectMapper();
         objectMapper.registerModule(new ParameterNamesModule());
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_EMPTY);
+        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         objectMapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
